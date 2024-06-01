b0424a3d6f12d43dbd60438ca8c738b0ccfeca7b
Prat123459 <123792371+Prat123459@users.noreply.github.com>
2024-06-02 00:33:18 +0530

    Update recommend.py

--- weatherforecastint/recommend.py
+++ weatherforecastint/recommend.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 df = pd.read_csv('D:/ThapliyalAgro-main/weatherforecastint/Crop_recommendation.csv')
 print(df.head())
 df.info()
