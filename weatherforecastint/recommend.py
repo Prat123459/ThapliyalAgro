b8c3b6c19207a838b152b52d376d44028cec5698
Prat123459 <123792371+Prat123459@users.noreply.github.com>
2024-06-02 00:36:23 +0530

    Update recommend.py

--- weatherforecastint/recommend.py
+++ weatherforecastint/recommend.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 df = pd.read_csv('D:/ThapliyalAgro-main/weatherforecastint/Crop_recommendation.csv')
 print(df.head())
+print(df.shape)
 df.info()
 print(df['label'].value_counts)
 x=df.drop('label',axis=1)
